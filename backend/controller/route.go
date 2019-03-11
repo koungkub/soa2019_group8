0dfb627d53625ffb4b684a1107bf9b7e348eb0b2
koungkub <1997jirasak@gmail.com>
2019-03-11 21:31:42 +0700

    add backend basic middleware

--- backend/controller/route.go
+++ backend/controller/route.go
@@ -4,12 +4,42 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 // Route : Create echo instance
 func Route() *echo.Echo {
 	e := echo.New()
 
+	// Basic middleware
+	e.Use(
+		middleware.Recover(),
+		middleware.Secure(),
+		middleware.BodyLimit("2M"),
+		middleware.GzipWithConfig(middleware.GzipConfig{
+			Level: 5,
+		}),
+		middleware.CORSWithConfig(middleware.CORSConfig{
+			AllowOrigins: []string{"*"},
+			AllowHeaders: []string{
+				echo.HeaderOrigin,
+				echo.HeaderContentType,
+				echo.HeaderAccept,
+				echo.HeaderAuthorization,
+				echo.HeaderAcceptEncoding,
+				echo.HeaderCookie,
+				echo.HeaderContentLength,
+			},
+			AllowMethods: []string{
+				http.MethodGet,
+				http.MethodPost,
+				http.MethodPut,
+				http.MethodDelete,
+			},
+		}),
+	)
+
+	// Health check path
 	e.GET("/_/health", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, echo.Map{
 			"message": "healthy",
