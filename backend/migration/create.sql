3a2d67f823e0ce6c7250907f8fcc514392a83d75
it59070073 <59070073@it.kmitl.ac.th>
2019-03-20 23:08:00 +0700

    edit sql command

--- backend/migration/create.sql
+++ backend/migration/create.sql
@@ -1,9 +1,16 @@
-CREATE TABLE iF NOT EXISTS `parking` (
-	id INT UNSIGNED NOT NULL KEY AUTO_INCREMENT,
-    status ENUM('parking', 'exit') NOT NULL,
-    rate INT UNSIGNED NOT NULL,
-    start_time DATETIME NOT NULL
+CREATE TABLE iF NOT EXISTS `DepartmentStore` (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  part_rate INT NOT NULL,
+  discount_rate INT NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
-DROP TABLE IF EXISTS `parking`
+CREATE TABLE iF NOT EXISTS `Parking` (
+  id INT NOT NULL AUTO_INCREMENT,
+  status ENUM('parking', 'exit') NOT NULL,
+  start_time DATETIME NOT NULL,
+  department_store_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_store_id) REFERENCES DepartmentStore (id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
