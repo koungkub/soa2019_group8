55706fa6b5bd3468ddf650e98460fa89e7fd5c7d
it59070073 <59070073@it.kmitl.ac.th>
2019-03-26 00:08:25 +0700

    add logging

--- parking/controller/parking.go
+++ parking/controller/parking.go
@@ -7,6 +7,8 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/sirupsen/logrus"
+
 	"github.com/koungkub/soa2019_group8/parking/service"
 
 	"github.com/labstack/echo"
@@ -17,14 +19,20 @@ func EntranceParking() echo.HandlerFunc {
 	return func(c echo.Context) error {
 
 		db := c.Get("db").(*sql.DB)
-		storeID, _ := strconv.Atoi(c.Param("id"))
+		storeID, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			logrus.Error("{id} only accept type int")
+			return echo.NewHTTPError(422, "{id} only accept type int")
+		}
 
 		id, err := service.EntranceParkingService(db, int64(storeID))
 		if err != nil {
-			return echo.NewHTTPError(422, "Can not entrance to parking")
+			logrus.Error("Can not entrance to parking, departmentstore id not found")
+			return echo.NewHTTPError(422, "Can not entrance to parking, departmentstore id not found")
 		}
 		token, err := service.GenerateToken(id, 24*time.Hour)
 		if err != nil {
+			logrus.Error("Generate token error")
 			return echo.NewHTTPError(422, "Generate token error")
 		}
 
