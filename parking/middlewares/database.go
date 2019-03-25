362c772b728883576ad23973afab7a431bf0436e
it59070073 <59070073@it.kmitl.ac.th>
2019-03-25 23:45:53 +0700

    delete log message  database middleware

--- parking/middlewares/database.go
+++ parking/middlewares/database.go
@@ -2,8 +2,8 @@ package middlewares
 
 import (
 	"database/sql"
+
 	"github.com/labstack/echo"
-	"github.com/sirupsen/logrus"
 )
 
 func DatabaseTransaction(db *sql.DB) echo.MiddlewareFunc {
@@ -11,9 +11,7 @@ func DatabaseTransaction(db *sql.DB) echo.MiddlewareFunc {
 		return echo.HandlerFunc(func(c echo.Context) error {
 
 			c.Set("db", db)
-
 			if err := next(c); err != nil {
-				logrus.Warnf("Database middleware failed, error is : %s", err)
 				return err
 			}
 			return nil
