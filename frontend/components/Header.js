f4e44016e65d4df42b7817e6720261825ec672ae
Chansin Thongkum <59070038@it.kmitl.ac.th>
2019-03-26 15:17:20 +0700

    add header component

---
+++ frontend/components/Header.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import { Fragment } from 'react';
+export default () => (
+  <Fragment>
+    <Head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+  crossOrigin="anonymous"/>
+    </Head>
+  </Fragment>
+)
