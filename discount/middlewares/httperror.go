4a096ffadeb9de00399738210da754a0f7e7db82
koungkub <1997jirasak@gmail.com>
2019-04-01 22:30:50 +0700

    add central log error

--- discount/middlewares/httperror.go
+++ discount/middlewares/httperror.go
@@ -2,12 +2,14 @@ package middlewares
 
 import (
 	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
 )
 
 // HTTPErrorHandler : Handle error was happend in service
 func HTTPErrorHandler(err error, c echo.Context) {
 
 	if errors, ok := err.(*echo.HTTPError); ok {
+		logrus.Error(errors.Message)
 		c.JSON(errors.Code, echo.Map{
 			"message": errors.Message,
 		})
