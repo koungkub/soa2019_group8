0f671ed5460d234dcf9b895cf22ca16241a7bd78
Chansin Thongkum <59070038@it.kmitl.ac.th>
2019-04-15 02:04:40 +0700

    create discount btn

---
+++ frontend/components/DiscountButton.js
@@ -0,0 +1,34 @@
+import React, { Component, Fragment } from 'react';
+import Link from 'next/link'
+import PropTypes from 'prop-types';
+import {withStyles, Button, NoSsr} from '@material-ui/core';
+
+const styles = theme => ({
+  paymentBtn:{
+    fontSize: '2.9375rem',
+    background: 'green'
+  }
+});
+
+class DiscountButton extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Fragment>
+        <NoSsr>
+        <Button variant="contained"  color="primary"
+              size="large"
+              fontSize="large"
+              className={classes.paymentBtn}>
+                    PAYMENT
+              </Button>
+    </NoSsr>
+          </Fragment>
+    );
+  }
+}
+DiscountButton.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+ 
+export default withStyles(styles) (DiscountButton);
