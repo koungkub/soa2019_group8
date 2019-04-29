9dd917c57585e8e379aaf73449c53d43c0c3cc15
koungkub <1997jirasak@gmail.com>
2019-04-29 18:45:11 +0700

    edit script

--- discount/migration/migrate.sh
+++ discount/migration/migrate.sh
@@ -1,1 +1,1 @@
-docker run --net test_soa -v $PWD:/migrations migrate/migrate -path=/migrations/ -database mysql://discount:P@ssw0rd@tcp(mysql-discount:3306)/Discount up 1
+docker run --net test_soa -v $PWD:/migrations migrate/migrate -path=/migrations/ -database mysql://discount:P@ssw0rd@tcp\(mysql-discount:3306\)/Discount up 1
