a53f3a0d895f019713ec2e4c65fe35345588cf1a
Chansin Thongkum <59070038@it.kmitl.ac.th>
2019-05-01 22:38:32 +0700

    add parkgin func

--- frontend/function/parking.js
+++ frontend/function/parking.js
@@ -1,15 +1,12 @@
-import authen from './authen'
 import axios from 'axios'
 
 class Parking {
     getCurrentTime(){
-        if(authen.apply() == true){
-            axios.get(localStorage.rootapi + 'parking').then(
-                res=>{
-                    return res.data
-                }
-            )
-        } 
+        axios.get(localStorage.rootapi + 'parking').then(
+            res=>{
+                return res.data
+            }
+        ) 
     }
 }
 export default Parking
