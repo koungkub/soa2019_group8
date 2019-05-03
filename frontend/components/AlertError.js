f2abc74cc44f237c77fbeb355ce93f8b62318c05
Chansin Thongkum <59070038@it.kmitl.ac.th>
2019-05-04 04:26:49 +0700

    refactor

--- frontend/components/AlertError.js
+++ frontend/components/AlertError.js
@@ -1,34 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
-import Button from "@material-ui/core/Button";
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import ErrorIcon from "@material-ui/icons/Error";
-import InfoIcon from "@material-ui/icons/Info";
 import CloseIcon from "@material-ui/icons/Close";
-import green from "@material-ui/core/colors/green";
-import amber from "@material-ui/core/colors/amber";
 import IconButton from "@material-ui/core/IconButton";
 import Snackbar from "@material-ui/core/Snackbar";
-import SnackbarContent from "@material-ui/core/SnackbarContent";
-import WarningIcon from "@material-ui/icons/Warning";
+import SnackbarContent from "@material-ui/core/SnackbarContent"
 import { withStyles } from "@material-ui/core/styles";
 
 const variantIcon = {
-  success: CheckCircleIcon,
-  warning: WarningIcon,
-  error: ErrorIcon,
-  info: InfoIcon
+  error: ErrorIcon
 };
-function TransitionLeft(props) {
-  return <Slide {...props} direction="left" />;
-}
 const styles1 = theme => ({
   error: {
     backgroundColor: theme.palette.error.dark
   },
   icon: {
-    fontSize: 20
+    fontSize: 50
   },
   iconVariant: {
     opacity: 0.9,
@@ -36,12 +24,13 @@ const styles1 = theme => ({
   },
   message: {
     display: "flex",
+    fontSize:20,
     alignItems: "center"
   }
 });
 
 function MySnackbarContent(props) {
-  const { classes, className, message, onClose, variant, ...other } = props;
+  const { classes, className, message, onClose, variant } = props;
   const Icon = variantIcon[variant];
 
   return (
@@ -51,7 +40,7 @@ function MySnackbarContent(props) {
       message={
         <span id="client-snackbar" className={classes.message}>
           <Icon className={classNames(classes.icon, classes.iconVariant)} />
-          {message}
+            {message}
         </span>
       }
       action={[
@@ -65,7 +54,6 @@ function MySnackbarContent(props) {
           <CloseIcon className={classes.icon} />
         </IconButton>
       ]}
-      {...other}
     />
   );
 }
