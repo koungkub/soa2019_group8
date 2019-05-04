5b9083d0ed9c1452e235dbbdba09a3128c3a9ff4
Chansin Thongkum <59070038@it.kmitl.ac.th>
2019-05-04 18:34:29 +0700

    refactor

--- frontend/components/AlertText.js
+++ frontend/components/AlertText.js
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
-import { SnackbarProvider, withSnackbar } from 'notistack';
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { SnackbarProvider, withSnackbar } from 'notistack'
 
 class AlearText extends React.Component {
     componentDidMount(){
@@ -10,31 +10,31 @@ class AlearText extends React.Component {
         this.props.onRef(undefined)
     }
   handleClickInfo () {
-    this.props.enqueueSnackbar('Please wait . . .', {variant: 'info'});
-  };
+    this.props.enqueueSnackbar('Please wait . . .', {variant: 'info'})
+  }
   handleClickError () {
-    this.props.enqueueSnackbar('Something error. please try again', {variant: 'error'});
-  };
+    this.props.enqueueSnackbar('Something error. please try again', {variant: 'error'})
+  }
   render() {
     return (
         <Fragment>
         </Fragment>
-    );
+    )
   }
 }
 
 AlearText.propTypes = {
   enqueueSnackbar: PropTypes.func.isRequired,
-};
+}
 
-const MyAlearText = withSnackbar(AlearText);
+const MyAlearText = withSnackbar(AlearText)
 
 function IntegrationNotistack(props) {
   return (
     <SnackbarProvider maxSnack={3}>
       <MyAlearText onRef={props.onRef}/>
     </SnackbarProvider>
-  );
+  )
 }
 
-export default IntegrationNotistack;
+export default IntegrationNotistack
