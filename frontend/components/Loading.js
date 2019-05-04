56d362c0cb08db8cb9c08e4ba697af9d2c219bfd
Chansin Thongkum <59070134@it.kmitl.ac.th>
2019-05-04 18:38:47 +0700

    refactor

--- frontend/components/Loading.js
+++ frontend/components/Loading.js
@@ -1,5 +1,5 @@
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { Component, Fragment } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { Component, Fragment } from 'react'
 
 class Loading extends Component {
 
@@ -12,4 +12,4 @@ class Loading extends Component {
     }
 }
   
-export default  (Loading);
+export default  (Loading)
